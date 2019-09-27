051ab5da6ffac03f977a11956caf5d12fe8df807
MicahJank <MicahJank@users.noreply.github.com>
2019-09-27 11:51:02 -0500

    added the logic for the different action types in the movieList reducer

--- studio-ghibli/src/reducers/movieList.js
+++ studio-ghibli/src/reducers/movieList.js
@@ -1,15 +1,34 @@
 // import action variables
+ import { FETCH_MOVIES_FAIL, FETCH_MOVIES_SUCCESS, FETCH_MOVIES_PENDING } from '../actions'; 
 
 
 const initialState = {
     movies: [],
     error: '',
-    isFetching: false
+    isPending: false
 }
 
 export const movieListReducer = (state = initialState, action) => {
     switch(action.type) {
-
+        case FETCH_MOVIES_PENDING:
+            return {
+                ...state,
+                isPending: true,
+                error: ''
+            }
+        case FETCH_MOVIES_SUCCESS:
+            return {
+                ...state,
+                movies: action.payload,
+                error: '',
+                isPending: false
+            }
+        case FETCH_MOVIES_FAIL:
+            return {
+                ...state,
+                isPending: false,
+                error: action.payload
+            }
         default:
             return state;
     }
