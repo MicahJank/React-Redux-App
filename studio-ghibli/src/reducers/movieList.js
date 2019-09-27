7606aee7747d0bb5b7a5b36b562b7d8eec8ccc48
MicahJank <MicahJank@users.noreply.github.com>
2019-09-26 21:00:26 -0500

    got most of the boilerplate out of the way

---
+++ studio-ghibli/src/reducers/movieList.js
@@ -0,0 +1,17 @@
+// import action variables
+
+
+const initialState = {
+    movies: [],
+    error: '',
+    isFetching: false
+}
+
+export const movieListReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        default:
+            return state;
+    }
+
+}
